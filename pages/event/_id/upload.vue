e6aa71db190c48132e97b7b0289946b99547ae9b
appleRui <apple31581@gmail.com>
2021-12-01 18:35:59 +0900

    Add/211201/FilePond Vali ImagePrev

--- pages/event/_id/upload.vue
+++ pages/event/_id/upload.vue
@@ -12,12 +12,53 @@
         >
       </v-card>
 
+      <file-pond
+        name="image-upload-form"
+        label-idle="ドラック＆ドロップしてね"
+        v-bind:allow-multiple="true"
+        accepted-file-types="image/jpeg, image/png"
+        :files="myFiles"
+        @init="handleFilePondInit"
+      />
     </v-container>
   </v-main>
 </template>
 
 <script>
-export default {};
+// Import Vue FilePond
+import vueFilePond from "vue-filepond";
+
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+
+// Import FilePond plugins
+// Please note that you need to install these plugins separately
+
+// Import image preview plugin styles
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+
+// Import image preview and file type validation plugins
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+
+// Create component
+const FilePond = vueFilePond(
+  FilePondPluginFileValidateType,
+  FilePondPluginImagePreview
+);
+
+export default {
+  methods: {
+    handleFilePondInit: function () {
+      console.log("FilePond has initialized");
+
+      // FilePond instance methods are available on `this.$refs.pond`
+    },
+  },
+  components: {
+    FilePond,
+  },
+};
 </script>
 
 <style>
